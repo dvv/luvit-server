d0094b41b8a3b907c3cc610915847186b8a7bcf7
Vladimir Dronnikov <dronnikov@gmail.com>
2011-12-12 22:47:08 +0400

    package.lua

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+  name = 'server',
+  version = '0.0.1',
+  description = "Boilerplate for webs server in Luvit",
+  author = "Vladimir Dronnikov <dronnikov@gmail.com>",
+  dependencies = {
+    json    = "https://github.com/luvit/json/zipball/master",
+    crypto  = "https://github.com/luvit/crypto/zipball/master",
+    uuid    = "https://github.com/luvit/uuid/zipball/master",
+  },
+}
